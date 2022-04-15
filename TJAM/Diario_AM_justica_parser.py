4e8074e82795f3793db4f7fa3e1dd411b39b0c12
saylon1986 <saylon.pereira@gmail.com>
2022-04-15 19:25:17 -0300

    ajustes parser e readme

--- TJAM/Diario_AM_justica_parser.py
+++ TJAM/Diario_AM_justica_parser.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 from PyPDF2 import PdfFileReader, PdfFileMerger
 import os, re
 import fitz
+import random
 
 
 
@@ -12,11 +13,10 @@ import fitz
 
 
 
-def Separar_textos_paginas():
+def Separar_textos_paginas(ano):
 
 	# seleciona a pasta de acordo com o ano
-	anos_di = input("Digite o ano(ex: 2003):")
-	diret = r'.\Diarios_AM_'+anos_di
+	diret = r'.\Diarios_AM_'+ano
 
 
 	# listas as pastas
@@ -30,6 +30,9 @@ def Separar_textos_paginas():
 	vlrs_unific= [] # lista que salva os valores que identificam os parágrafos
 	txt_unific = []
 	sem_lines = []
+
+	## lista para verificar as flags escolhidas
+	caracteristicas =[]
 
 
 	# iteração sobre os arquivos dentro das pastas
@@ -59,6 +62,7 @@ def Separar_textos_paginas():
 
 								# itera para cada linha
 
+							txt_block = []
 							for x in range(len(lines)):
 
 								# dentro das linhas seleciona os spans
@@ -66,25 +70,70 @@ def Separar_textos_paginas():
 								spans = lines[x]["spans"]
 								
 								# para cada spam:
-
+								
 								for u in spans:
-									te = str(u['bbox'][0]) # seleciona o valor do parágrafo
-									ta = te.split(".") # corta no decimal
-									dist = int(ta[0]) # separa apenas os primeiros dígitos antes do ponto
-									vlrs_unific.append(dist) # coloca esse valor na lista
-									txt_unific.append(u['text'].strip()) # coloca também o texto na lista
-									numeros_paginas.append(num_pag) # salva o número da página desse texto
-									nomes_pastas.append(nome_pasta) # salva o nome da pasta 
-									nome_doc.append(arquivos[a]) # salva o nome do documento
+
+									tam = str(u['size']).split(".")[0]
+									flag =str(u['flags'])
+
+									## para o teste previo de verificar as flags	
+									caracteristicas.append((tam,flag))
+									
+									if tam == "7" and flag == "4" or tam == "7" and flag =="20": 
+										txt_block.append(u['text'].strip()) # separa todos os textos de cada bloco e salva na lista para unificação
+								
+
+
+									## para verificar o que aparece nos padrões das flags
+							
+									# if tam == "6" and flag == "4":
+									# 	print("\n\n PADRÃO 2\n\n",u['text'])
+									# 	z = input("")
+									# # if tam == "9" and flag == "4":
+									# 	print("\n\n PADRÃO 3\n\n",u['text'])	
+									# 	z = input("")
+									
+									
+							# unifica os blocos dos textos, salvando também os números das páginas, pastas e arquivos								
+
+							if len(txt_block) > 0:
+								
+								txt_fim = " ".join(txt_block)
+								txt_unific.append(txt_fim)
+								numeros_paginas.append(num_pag)
+								nomes_pastas.append(nome_pasta[-10:])
+								nome_doc.append(arquivos[a])
+							
+							# caso o texto do bloco seja vazio, unifica um texto vazio para manter a mesma quantidade d eitens da lista
+							else:
+								txt_fim = " "
+								txt_unific.append(txt_fim)
+								numeros_paginas.append(num_pag)
+								nomes_pastas.append(nome_pasta[-10:])
+								nome_doc.append(arquivos[a])
+								
 						
 						# caso não tenha a seção lines salva nessa outra lista o número do bloco (sem utilidade)
 						except:
 							sem_lines.append(blocks[o]["number"])
 
 
-	# função que faz a junção dos blocos com base no valor do parágrafo
+	## contabilização da quantidade de flags mais frequentes
+							
+	# nome_acao = pd.DataFrame()
+	# nome_acao["Ação"] = caracteristicas							
+	# nome_acao = pd.DataFrame(nome_acao.groupby(["Ação"])["Ação"].count())
+	# nome_acao.columns = ["quantidade"]
+	# nome_acao = nome_acao.reset_index()						
 
-	Juntar_blocks(numeros_paginas,nome_doc, nomes_pastas, vlrs_unific, txt_unific)								
+	# print(nome_acao.sort_values(by=['quantidade'],ascending=False))
+	# z = input("")
+
+
+	# função que faz a junção dos blocos com base no valor do parágrafo
+	# print("Temos",len(txt_unific),"publicações")
+
+	Juntar_blocks(numeros_paginas,nome_doc, nomes_pastas, txt_unific, ano)								
 	
 	# retorna as listas (antes de fazer a junção)
 	# return numeros_paginas, nome_doc, nomes_pastas, vlrs_unific, txt_unific
@@ -93,117 +142,154 @@ def Separar_textos_paginas():
 
 ###############################  Função para juntar os blocos dos textos ################################################
 
-def Juntar_blocks(numeros_paginas,nome_doc, nomes_pastas, vlrs_unific, txt_unific):
-
-		#listas com os valores a serem aproveitados
-		prgf = [] # index da lista com os textos das publicações que são parágrafos
+def Juntar_blocks(numeros_paginas,nome_doc, nomes_pastas, txt_unific,ano):
 
 
-		for k in range(len(vlrs_unific)): # itera sobre a lista com os valores dos parágrafos
-			if vlrs_unific[k] == 70 or vlrs_unific[k] == 316 or vlrs_unific[k] == 317: # se forem estes valores (valores dos parágrafos)
-				pos = vlrs_unific.index(vlrs_unific[k],k) # verifica o index daquele elemento, sempre começando a buscar a partir dele na lista 
-				prgf.append(pos) # coloca o index numa lista
+	publicacoes = []
+	num_pags = []
+	nome_docs = []
+	nome_pst = []
+	num_process =[]
+
+
+	for txt,num,doc,pst in zip(txt_unific,numeros_paginas,nome_doc,nomes_pastas):
+		
+		####        identifiquei esse problema de encoding do traço (-) no texto em alguns números cnj no pdf, o que enganava o regex  #########
+
+								
+											#  Número:  1001461­41.2022.8.11.0000 
+
+								
+										########                                 ###########
 
 		
-		# listas com os dados finais a serem incluídos no BD
-
-		publicacoes = [] 
-		num_paginas= []		
-		nom_pastas = []
-		nom_docs = []
-		
-
-		# itera sobre a lista com os index dos parágrafos
-
-		z = 0
-		while True:
-
-			if z >= len(prgf)-1:
-				break
-			else:
-
-				atual = prgf[z] # recebe o index do primeiros
-				seguinte = prgf[z+1] # recebe o index do segundo
+		# caso encontra a o padrão CNJ na publicação ele separa a publicação, o número da página, documento e pasta.
+		# pelo problema acima do encoding relatado acima adotei o regex somente da parte final do padrão CNJ (ex: 42.2021.8.11.0000)
 
 
-				if seguinte == atual+1: # se o segundo for consecutivo do primeiro
-					# print(atual, seguinte,"são consecutivos! O z valia",z)
-					# s = input("")
+		## regra da pesquisa do número CNJ dentro do texto da publicação
 
-					while True: 
-					
-						z = z+1 # z aumenta um elemento
-						teste = prgf[z] # teste recebe novo elemento
-						if z >= len(prgf)-1:
-							break
-						else:
-							seguinte = prgf[z+1] # #seguinte recebe o elemento seguinte ao anterior
-							# print(teste,seguinte,"são os atuais. O Z vale",z)
-							if teste+1 != seguinte: # verifica se ainda são consecutivos
-								# print("acabaram os consecutivos")
-								# print("o atual será cortado", atual,"e o seguinte será cortado em", 
-								# 	seguinte)
-								# s = input("")
-								break 
+		if len(txt) <= 1000: # se a publicação tiver até 1000 caracteres, procura no texto todo
+			text = txt
+		else:	
+			vlr = int(len(txt)*0.10)
+			if vlr < 350: # se tiver mais de mil até 3500, procura nos 350 primeiro caracteres
+				vlr = 350
+			text = txt[0:vlr] # fora isso, pesquisar nos 10% primeiros caracteres da publicação
 
 
-					publi = txt_unific[atual:seguinte] # corta a lista entre esses elementos
-					txt_publi = " ".join(publi) #unifica os textos nesse intervalo
+		## incício da busca
 
-					# if len(txt_publi) > 150: # eliminar parágrafos e outras coisas que não são publicações
-					publicacoes.append(txt_publi) # salva o texto da publicação
-					num_paginas.append(numeros_paginas[atual]) # o número da pagina onde começa
-					nom_pastas.append(nomes_pastas[atual][-10:]) # o nome da pasta
-					nom_docs.append(str(nome_doc[atual])) # e do documento
-					z = z+1
+		if re.search(r'\d{2}\.\d{4}\.\d\.\d{2}\.\d{4}',text, re.IGNORECASE.MULTILINE): # pesquisa o padrão em todas as linhas da publicação (dentro do limite de caracteres)
+			try:
+				nm_proc = re.search(r'\d{2,7}-\d{2}\.\d{4}\.\d\.\d{2}\.\d{4}',text, re.IGNORECASE.MULTILINE).group() # se encontrar o padrão completo, separa o número
+				num_process.append(nm_proc) # salva na lista
 
-
+			# se encontrou so o padrão parcial por causa do problema acima
+			except:
+				posic = re.search(r'\d{2}\.\d{4}\.\d\.\d{2}\.\d{4}',text, re.IGNORECASE.MULTILINE).span() # pega os caracter do início e o fim do padrão parcial 
+				
+				# se o número do caracter não foi menor do que o início da publicação
+				if posic [0] - 10 >= 0:
+					trecho = text[posic [0] - 10:posic[1]].strip() # separa o trecho voltando 10 caracteres do início encontrado (a parte inicial do nº cnj tem até 7 digitos)
 				else:
-					publi = txt_unific[atual:seguinte] # corta a lista entre esses elementos
-					txt_publi = " ".join(publi) #unifica os textos nesse intervalo
+					# se for menor e houve algum erro na quebra da página, recebe o número parcial mesmo
+					trecho =  re.search(r'\d{2}\.\d{4}\.\d\.\d{2}\.\d{4}',text, re.IGNORECASE.MULTILINE).group()
 
-					# if len(txt_publi) > 150: # eliminar parágrafos e outras coisas que não são publicações
-					publicacoes.append(txt_publi) # salva o texto da publicação
-					num_paginas.append(numeros_paginas[atual]) # o número da pagina onde começa
-					nom_pastas.append(nomes_pastas[atual][-10:]) # o nome da pasta
-					nom_docs.append(str(nome_doc[atual])) # e do documento
-					z = z+1
+				nm_proc = re.search('(\d.*\.\d{4})',trecho).group() # elimina eventuais sobras de texto que tenham sido captadas
+				num_process.append(nm_proc)  # salva na lista o número limpo
 
-
-		### conferir o número das páginas		
-
-		# for item in publicacoes:
-		# 	print("------------------------")
-		# 	print(item)
-		# 	s = input("")
+			# salva nas listas a publicação e as demais informações dela (página, documento, pasta)	
+			publicacoes.append(txt) 
+			num_pags.append(num)
+			nome_docs.append(doc)
+			nome_pst.append(pst)
 
 
-
-		# Transforma num DF		
-
-		df_textos_paginas = pd.DataFrame()    
-		df_textos_paginas["publicacoes"] = publicacoes
-		df_textos_paginas["numeros_paginas"] = num_paginas
-		df_textos_paginas["nome_documento"] = nom_docs
-		df_textos_paginas["nomes_pastas"] = nom_pastas	
-
-		try:
-			antigos = pd.read_excel("Diarios_publicacoes.xlsx", engine ='openpyxl')
-			# print("já temos uma planilha!")
-			df_textos_paginas = pd.concat([antigos,df_textos_paginas])
-			df_textos_paginas.to_excel("Diarios_publicacoes.xlsx", index = False)
-		except:
-			df_textos_paginas.to_excel("Diarios_publicacoes.xlsx", index = False)
-		
+			## comparação das duas últimas publicações:
+			if len(num_process) >= 2: # só da pra fazer isso depois que tivermos pelo menos dois números na lista
+				ultimo = num_process[-1]
+				penultimo = num_process [-2]
+				if ultimo == penultimo: # se as duas tiverem o mesmo número CNJ
+					unif = publicacoes[-2]+' '+publicacoes[-1] #unifica as duas na penúltima
+					
+					#deleta tudo da última publicação inserida
+					del num_process [-1] 
+					del publicacoes [-1]
+					del num_pags [-1]
+					del nome_docs [-1]
+					del nome_pst [-1]		
 	
+	
+		# caso ele não encontre o padrão CNJ e essa publicação não seja a primeira da lista 
+		else:
+			if len(publicacoes)>=1 and re.search("^Disponibilizado -",txt,re.IGNORECASE) == None: #verifica se atingiu a quantidade máxima de unificações (4) sem encontrar um padrão CNJ ou se é a primeira da lista
+				txt = publicacoes[-1]+" "+txt  # unifica o texto atual com a publicação anterior
+				del publicacoes[-1] # deleta da lista a publicação anterior
+				publicacoes.append(txt) # junta a nova publicação unificada na lista (o número da página e o nome do doc se mantém onde a publicação começa)
+				
+		
+
+
+	## PARA CONFERÊNCIA - DESCOMENTAR CASO QUEIRA VERIFICAR O CORTE FINAL DAS PUBLICAÇÕES NA ORDEM - APERTAR ENTER A CADA PUBLICAÇÃO
+	# qtdade = 0
+	# for item,num in zip(publicacoes,num_pags):
+	# 	qtdade = qtdade+1
+	# 	print("Quantidade avaliada:",qtdade)
+	# 	print("página", num)
+	# 	print(item)
+	# 	print("-----------------")
+	# 	z = input('')
+
+
+
+	# gera o DF com as publicações e as demais informações
+
+	df_textos_paginas = pd.DataFrame()
+	df_textos_paginas["Número do processo"] = num_process
+	df_textos_paginas["publicacoes"] = publicacoes
+	df_textos_paginas["numeros_paginas"] = num_pags
+	df_textos_paginas["nome_documento"] = nome_docs
+	df_textos_paginas["nomes_pastas"] = nome_pst	
+
+	
+
+
+	### CONFERÊNCIA AMOSTRAL ALEATÓRIA - DESCOMENTAR CASO QUEIRA UMA AMOSTRA ALEATÓRIA DOS RECORTES  - APERTAR ENTER A CADA PUBLICAÇÃO
+
+
+	# # agrupa por nome do documento
+	doc_agrup = pd.DataFrame(df_textos_paginas.groupby(["nome_documento"])["nome_documento"].count())
+	doc_agrup.columns = ["quantidade"]
+	doc_agrup = doc_agrup.reset_index()
+
+	# converte os nomes em uma lista e depois embaralha os nomes em uma ordem indeterminada
+	lista_nomes_docs = doc_agrup["nome_documento"].tolist()
+	random.shuffle(lista_nomes_docs)
+
+
+	# Gera uma amostra aleatória de X publicações por documento para facilitar a conferência
+	for docu in lista_nomes_docs :
+		df_filter = df_textos_paginas["nome_documento"] == docu
+		amostra_trib = df_textos_paginas[df_filter]
+
+		amostra_trib = amostra_trib.sample(10)  # escolher a quantidade da amostra
+		for pub,doc,pag in zip(amostra_trib["publicacoes"],amostra_trib["nome_documento"],amostra_trib["numeros_paginas"]):
+			print("documento:\t",doc,"\nPágina:\t",pag,"\nTexto publicação:\n",pub,"\n--------------")
+			z= input("")
+
+
+	# gera o excel com o DF final
+
+	df_textos_paginas.to_excel("Diarios_publicacoes_AM_"+ano+".xlsx", index = False)
 
 
 
 ################################################################################################################
 
 def Main_Separacao():
-
-	data_frames = Separar_textos_paginas()
+	ano = input("Digite o ano com 4 dígitos (Ex:2012):")
+	data_frames = Separar_textos_paginas(ano)
 
 ################################################################################################################
 
--- TJAM/README.md
+++ TJAM/README.md
@@ -1,4 +1,8 @@
-### TJAM - Metodologia
+# TJAM 
+
+![Imagem do diario](image_diario_AM.png)
+
+## Metodologia
 
 #### 1. Coleta
 
@@ -13,34 +17,66 @@ O formato da data a ser inserida é MÊS-DIA-ANO
 Recomedamos selecionar o período de um ano por vez.
 
 
-#### 2. Parser
+#### 2. Parser das publicações
+
+Para dar inicio ao processo é necessário inserir o ano da pasta a ser "parseada" no formato de 4 dígitos.
+
+Este parser leva em consideração para realizar os recortes os blocos na forma como eles vem organizados no documentos,
+já que nesse casos eles já estão organizados, em sua grande maioria, da forma correta.
+
+Utilizando o python é possível acessar o texto na formato da sua estrutura gerando o seguinte resultado:
+
+*{'number': 0, 'type': 0, 'bbox': (24.0, 361.0083312988281, 85.38816833496094, 368.7727966308594), 'lines': [{'spans': [{**'size': 6.949999809265137, 'flags': 16**, 'font': 'Arial,Bold', 'color': 0, 'ascender': 0.9052734375, 'descender': -0.2119140625, **'text': 'TRIBUNAL PLENO'**, 'origin': (24.0, 367.29998779296875), 'bbox': (24.0, 361.0083312988281, 85.38816833496094, 368.7727966308594)}], 'wmode': 0, 'dir': (1.0, 0.0), 'bbox': (24.0, 361.0083312988281, 85.38816833496094, 368.7727966308594)}]}*
 
 
-Este parser leva em consideração para realizar os recortes as posições dos elementos no texto, 
-tendo como norte as posições mais frequentes dos parágrafos, que são 70, 316 e 317.
+Essa estrutura de um dicionario também é manuseada no python, do qual são extraídos os elementos indicados em negrito - *text, size* e *flag*. A escolha por manusear
+esse elementos foi para filtrar, de saída, os textos que interessavam dos conteúdos desnecessários, como como indíces, caixas de texto de seções, cabeçalhos, etc.
 
-As linhas são unificadas em bloco de acordo com os valores dos parágrafos, servindo estes de marcadores
-de início e fim.
+O atributo *'text'* guarda o texto que visualizamos no PDF. O exemplo acima possui apenas um título, mas nas publicações temos uma série de spans com
+inúmeras linhas de textos dentro delas.
 
-Caso haja vários parágrafos consecutivos na lista, eles são unificados juntos.
+Analisando os atributos *size* e *flag* mais frequentes nos diários desse tribunal, notamos a seguinte frequência: 
+
+Index |  Tupla  | Quantidade |
+----- | ------- |  --------  |
+  1	  |*(7, 4)* |  *368497*  |
+  2   |*(7, 20)*|   *13486*  |
+  3   | (5, 4)  |     7731   |
+  4   | (6, 4)  |     3992   |
 
 
+ __(recortamos apenas o valor antes do ponto (.) e unimos um tupla com size e flag, respectivamente; organizando a frequência como descendente e escolhendo os 4 primeiros)__
 
-#### Ajustes:
+Como se nota, destaca-se muito nos textos a tupla *size/flag* nos valores *7* e *4*. Isso indica que a maioria dos textos do diário possui essa configuração. Contudo, analisando as demais verificamos que também a tuplas *7/20* podem trazer informações do processo contendo textos que fazem parte da publicação.
 
-O procedimento acima resolveu boa parte dos problemas de recorte, mas ainda temos casos em que as seções
-unificam muitos parágrafos quebrados, gerando situações como a mostrada abaixo:
+Com base nesse estudo, foram selecionados dos diários apenas os textos com as formatações 1 e 2.
+
+De posse dos textos das publicações o recorte atendeu ao seguinte critério:
+
+1. Separamos a parte inicial da publicação - detalhes de como isso foi feito está no código
+2. Buscamos o padrão CNJ nesse trecho do texto
+3. Caso encontrado, salva a publicação e compara o número CNJ com o da publicação anterior(se houver).
+	- Se os números forem iguais, unifica as publicações e elimina a última
+	- Se forem diferentes, mantem as duas.
+4. Caso ele não encontre o padrão CNJ no começo da publicação, ele unifica as publicações sem esse padrão com a anterior
+até encontrar a próxima publicação com o padrão CNJ. Esse procedimento permite que uma publicação dividida em várias
+seções seja unificada.
 
 
-------------------------
-Conclusão de Acórdãos Processo: 0210980-41.2018.8.04.0022 - Correição Extraordinária,  Juiz Corregedor Auxiliar - Setor 2
+### Margem de erro:
 
-------------------------
-Corrigente: Exmo. Sr. Corregedor Geral de Justiça do Estado do Amazonas
+Nos testes que realizamos tivemos os seguintes resultados:
 
-------------------------
-Presidente:  Yedo Simões de Oliveira. Relator:  Djalma Martins da Costa. Revisor:  Revisor do processo Não informado
+1. Teste seguindo a ordem normal dos documentos:
 
-------------------------
-EMENTA: RELATÓRIO DE CORREIÇÃO EXTRAORDINÁRIA REALIZADA NA VARA 1.ª VARA DA COMARCA DE TEFÉ. HOMOLOGAÇÃO- A presente Correição extraordinária merece ser homologada,  porquanto reﬂ ete com ﬁ delidade os fatos narrados em sua missão, além de ter realizado minucioso trabalho nos autos dos processos examinados, livros e documentos existentes no Cartório da 1.ª Vara da Comarca de Tefé, consoante as formalidades legais exigidas;- Quanto à recomendações feitas, tenho-as como adequadas ao resultado da inspeção, merecendo destaque as que objetivam o aperfeiçoamento das condições físicas do Fórum de Justiça da Comarca de Tefé, bem como a designação de juízes, com exclusividade, e servidores para atuarem nas respectivas unidades judiciárias da referida Comarca, Publicação Oﬁ cial do Tribunal de Justiça do Estado do Amazonas - Lei Federal nº 11.419/06, art. 4º Disponibilização: quinta-feira, 7 de março de 2019 Diário da Justiça Eletrônico - Caderno Judiciário - Capital Manaus, Ano XI - Edição 2568 3 motivo porque acolho todas as recomendações sugeridas. - Relatório aprovado e homologado. .  DECISÃO: “Por unanimidade, o Egrégio Tribunal Pleno decidiu aprovar e homologar a Correição Extraordinária realizada no Cartório da 1.ª Vara da Comarca de Tefé, observadas as formalidades legais pertinentes à espécie, nos termos do voto do Reltor.”.  Sessão: 12 de fevereiro de 2019.
+- Quantidade de publicações:
+- Quantidade verificada: X primeiras
+- Erros e faltantes: X
+- Anos testados: 2019
 
+2. Teste de amostra aleatória:
+
+- Quantidade de publicações:
+- Quantidade verificada: X primeiras
+- Erros e faltantes: X
+- Anos testados: 2019
